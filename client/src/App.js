53a15dbeff0a2661d31c59e945f91977df0d20d7
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-07-14 20:25:42 +0700

    added client (unstable commit)

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={}/>
+        <Route exact path="/" component={}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/SignUpPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <div className="SignUp-Container">
+            
+        </div>
+    )
+}
+
+export default SignUp
